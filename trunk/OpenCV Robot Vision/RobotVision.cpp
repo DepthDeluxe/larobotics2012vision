e7778049b75cab9147ac86992a498b921aff25ee
cheinzmann3@gmail.com <cheinzmann3@gmail.com@23265881-9e3b-5c7c-a33c-db33ff920500>
2012-02-04 20:27:48 +0000

    Added: Build in release mode Fixed: Hough transforms properly lines at given theshold. Hough threshold no longer messes with canny detection.
    This is is the first fully functional build!

--- trunk/OpenCV Robot Vision/RobotVision.cpp
+++ trunk/OpenCV Robot Vision/RobotVision.cpp
@@ -1,21 +1,20 @@
 #include "RobotVision.h"
 
-RobotVision::RobotVision(int hi, int low, int hou)
+RobotVision::RobotVision(int lo, int hi, int hou)
 {
+	lowThreshold = lo;
 	highThreshold = hi;
-	lowThreshold = low;
 	houghThreshold = hou;
 }
 
 void RobotVision::Initialize()
 {
-	// init camera
+	// connect to camera
 	camera = cvCaptureFromCAM(0);
 
-	// init all variables
+	// initialize variables
 	image = cvQueryFrame(camera);
 	image_gray = cvCreateImage(cvGetSize(image), image->depth, 1);
-	brightFilterImage = cvCreateImage(cvGetSize(image), image->depth, 1);
 	cannyImage = cvCreateImage(cvGetSize(image), image->depth, 1);
 
 	storage = cvCreateMemStorage(0);
@@ -23,46 +22,34 @@ void RobotVision::Initialize()
 
 void RobotVision::GetNextFrame()
 {
-	// get the next camera frame
+	// get frame
 	image = cvQueryFrame(camera);
 
 	// convert to black and white
 	cvCvtColor(image, image_gray, CV_BGR2GRAY);
+
+	// do histogram equalizing on gray image
+	cvEqualizeHist(image_gray, image_gray);
 }
 
 void RobotVision::FilterPass()
 {
-	// do histogram equalizing on gray image
-	cvEqualizeHist(image_gray, image_gray);
-
-	// do brightness filter
-	cvThreshold(image_gray, image_gray, brightThreshold, 255, CV_THRESH_BINARY);
-
-	// do canny filtering
+	// do canny
 	cvCanny(image_gray, cannyImage, lowThreshold, highThreshold, 3);
 }
 
 void RobotVision::TransformPass()
 {
-	// perform hough transform and save it to lineBuffer
-	//lineBuffer = cvHoughLines2(filtered, storage, CV_HOUGH_STANDARD, 1, CV_PI/180, houghThreshold, 0, 0);
-	lineBuffer = cvHoughLines2(cannyImage, storage, CV_HOUGH_STANDARD, 1, CV_PI/(180*0.5), houghThreshold, 0, 0);
+	// do standard hough transform
+	lineBuffer = cvHoughLines2(cannyImage, storage, CV_HOUGH_STANDARD, 1, CV_PI / 180, houghThreshold);
 }
 
-void RobotVision::DrawHoughLines(int imgtodrawon)
+void RobotVision::DrawHoughLines()
 {
-	bool isPerp = false;
-
 	for (int n = 0; n < lineBuffer->total; n++)
 	{
-		// get individual line from line buffer
+		// get a line from the buffer
 		float* line = (float*)cvGetSeqElem(lineBuffer, n);
-
-		isPerp = true;
-
-		// break out of iteration of loop if there is no perp
-		if (!isPerp)
-			continue;
 
 		// generate points from the line
 		double a = cos(line[1]);
@@ -77,12 +64,8 @@ void RobotVision::DrawHoughLines(int imgtodrawon)
 		pt2.x = cvRound(x0 - 1000*(-b));
 		pt2.y = cvRound(y0 - 1000*(a));
 
-		if (imgtodrawon == RV_DRAW_HOUGH_ON_RAW)
-			cvLine( image, pt1, pt2, CV_RGB(255,255,0), 3, 8 );
-		else
-			cvLine( cannyImage, pt1, pt2, CV_RGB(255,255,0), 3, 8 );
-
-		isPerp = false;
+		// draw line
+		cvLine( image, pt1, pt2, CV_RGB(255,255,0), 3, 8 );
 	}
 }
 
@@ -96,25 +79,24 @@ void RobotVision::SetHighThreshold(int value)
 	highThreshold = value;
 }
 
-void RobotVision::SetBrightThreshold( int value )
-{
-	brightThreshold = value;
-}
-
-void RobotVision::SetHoughThreshold( int value )
+void RobotVision::SetHoughThreshold(int value)
 {
 	houghThreshold = value;
 }
 
+IplImage* RobotVision::GetFilteredImage()
+{
+	return cannyImage;
+}
 
 IplImage* RobotVision::GetRawImage()
 {
 	return image;
 }
 
-IplImage* RobotVision::GetFilteredImage()
+IplImage* RobotVision::GetGrayImage()
 {
-	return cannyImage;
+	return image_gray;
 }
 
 void RobotVision::Dispose()
