5e43ab6e76881add751fcc4a06bc7f406256cd71
cheinzmann3@gmail.com <cheinzmann3@gmail.com@23265881-9e3b-5c7c-a33c-db33ff920500>
2012-03-11 20:18:39 +0000

    added command line options for runtime. added 'release mode' which basically removes all the launching of windows and stuff for efficiency!

--- trunk/OpenCV Robot Vision/main.cpp
+++ trunk/OpenCV Robot Vision/main.cpp
@@ -3,6 +3,9 @@
 
 #include <opencv/highgui.h>
 #include "RobotVision.h"
+#include "Utility.h"
+
+#include <conio.h>
 
 using namespace std;
 
@@ -44,73 +47,160 @@ void binaryThresholdCallback(int value)
 	threshChanged = true;
 }
 
-void main()
+void main(int argc, char* argv[])
 {
+	bool displayWindows = true;
+	bool debugOutput = false;
+
+	// process command line statements
+	for (int n = 1; n < argc; n++)
+	{
+		if (strcmp(argv[n], "-r") == 0)
+		{
+			displayWindows = false;
+		}
+
+		else if (strcmp(argv[n], "-do") == 0)
+		{
+			debugOutput = true;
+		}
+
+		else if (strcmp(argv[n], "?") == 0)
+		{
+			cout << "----- FRC 114 Robot Vision -----" << endl;
+			cout << "Commands:" << endl;
+			cout << "	-r: real runtime mode, windows" << endl;
+			cout << "	-do: enable udp debugging output, default port is 192.168.1.14" << endl;
+			cout << "		you can choose to enter another address after this" << endl;
+			cout << "--------------------------------" << endl;
+			return;
+		}
+
+		else
+		{
+			cout << "Error: you entered an invalid argument!" << endl;
+			cout << "	enter ? as first argument for list of" << endl;
+			cout << "	valid commands" << endl;
+			return;
+		}
+	}
+
+	cout<< "--- FRC 114 Robot Vision ----" << endl;
+	cout << "initializing..." << endl;
+
 	// load file
 	LoadData();
 
 	// create instance of robot vision and initialize
+	cout << "loading robot vision core..." << endl;
 	RobotVision robotVision(lowThresh, highThresh, houghThresh);
 	robotVision.Initialize();
 
-	// make windows
-	cvNamedWindow("OpenCV Window");
-	cvNamedWindow("Adjustment Window");
-	int key = 0;
+	// create instance of NetworkDebuggingOutput
+	cout << "starting debugging output on port 192.168.2.17..." << endl;
+	NetworkDebuggingOutput output("192.168.2.17", 6666);
 
-	// create trackbars for thresholding
-	cvCreateTrackbar("Low Threshold", "Adjustment Window", &lowThresh, 2000, lowThresholdCallback);
-	cvCreateTrackbar("High Threshold", "Adjustment Window", &highThresh, 2000, highThresholdCallback);
-	cvCreateTrackbar("Hough Threshold", "Adjustment Window", &houghThresh, 250, houghThesholdCallback);
-	cvCreateTrackbar("Binary Threshold", "Adjustment Window", &binaryThresh, 255, binaryThresholdCallback);
-
-	while (key != 'q')
+	if (displayWindows)
 	{
-		// get next frame
-		robotVision.GetNextFrame();
+		// make windows
+		cout << "creating view windows..." << endl;
+		cvNamedWindow("OpenCV Window");
+		cvNamedWindow("Adjustment Window");
+		int key = 0;
 
-		// send threshold values to function if bool is set to true
-		if (threshChanged)
+		// create trackbars for thresholding
+		cvCreateTrackbar("Low Threshold", "Adjustment Window", &lowThresh, 2000, lowThresholdCallback);
+		cvCreateTrackbar("High Threshold", "Adjustment Window", &highThresh, 2000, highThresholdCallback);
+		cvCreateTrackbar("Hough Threshold", "Adjustment Window", &houghThresh, 250, houghThesholdCallback);
+		cvCreateTrackbar("Binary Threshold", "Adjustment Window", &binaryThresh, 255, binaryThresholdCallback);
+
+		cout << "program started in debugging mode!" << endl;
+		cout << "-----------------------------" << endl << endl;
+		while (key != 'q')
 		{
-			robotVision.SetLowThreshold(lowThresh);
-			robotVision.SetHighThreshold(highThresh);
-			robotVision.SetHoughThreshold(houghThresh);
-			robotVision.SetBinaryThreshold(binaryThresh);
-			threshChanged = false;
+			// get next frame
+			robotVision.GetNextFrame();
+
+			// send threshold values to function if bool is set to true
+			if (threshChanged)
+			{
+				robotVision.SetLowThreshold(lowThresh);
+				robotVision.SetHighThreshold(highThresh);
+				robotVision.SetHoughThreshold(houghThresh);
+				robotVision.SetBinaryThreshold(binaryThresh);
+				threshChanged = false;
+			}
+
+			robotVision.DetectRectangle();
+			robotVision.LineAnalysis();
+
+			// draw functions
+			robotVision.DrawImportantRectangles();
+			robotVision.DrawRectangle();
+
+			// display the original image with hough lines on top of them
+			if (currentImageView == 1)
+				cvShowImage("OpenCV Window", robotVision.GetRawImage());
+			else if (currentImageView == 2)
+				cvShowImage("OpenCV Window", robotVision.GetThresholdImage());
+			else if (currentImageView == 3)
+				cvShowImage("OpenCV Window", robotVision.GetRegionOfInterestImage());
+
+			key = cvWaitKey(20);
+
+			if (key == '1')
+				currentImageView = 1;
+			else if (key == '2')
+				currentImageView = 2;
+			else if (key == '3')
+				currentImageView = 3;
+
+			// send to debugging output
+			if (debugOutput)
+				output.Send(robotVision.GetRectangleInformation());
 		}
+	}
+	else
+	{
+		cout << "program started in release mode!" << endl;
+		cout << "-----------------------------" << endl << endl;
 
-		robotVision.DetectRectangle();
-		robotVision.LineAnalysis();
+		// set the thresholds
+		robotVision.SetLowThreshold(lowThresh);
+		robotVision.SetHighThreshold(highThresh);
+		robotVision.SetHoughThreshold(houghThresh);
+		robotVision.SetBinaryThreshold(binaryThresh);
 
-		// draw functions
-		robotVision.DrawImportantRectangles();
-		robotVision.DrawRectangle();
+		int key = 0;
 
-		// display the original image with hough lines on top of them
-		if (currentImageView == 1)
-			cvShowImage("OpenCV Window", robotVision.GetRawImage());
-		else if (currentImageView == 2)
-			cvShowImage("OpenCV Window", robotVision.GetThresholdImage());
-		else if (currentImageView == 3)
-			cvShowImage("OpenCV Window", robotVision.GetRegionOfInterestImage());
+		while (key != 'q')
+		{
+			robotVision.GetNextFrame();
+			robotVision.DetectRectangle();
+			robotVision.LineAnalysis();
 
-		key = cvWaitKey(20);
+			if (debugOutput)
+				output.Send(robotVision.GetRectangleInformation());
 
-		if (key == '1')
-			currentImageView = 1;
-		else if (key == '2')
-			currentImageView = 2;
-		else if (key == '3')
-			currentImageView = 3;
+			if (kbhit() == 1)
+				key = getch();
+		}
 	}
 
+	cout << endl << "-----------------------------" << endl;
+	cout << "closing..." << endl;
+
 	// release objects
-	cvDestroyWindow("OpenCV Window");
-	cvDestroyWindow("Adjustment Window");
+	if (displayWindows)
+	{
+		cvDestroyWindow("OpenCV Window");
+		cvDestroyWindow("Adjustment Window");
+	}
 	robotVision.Dispose();
 
-	// save file
-	SaveData();
+	// save file only if windows are shown
+	if (displayWindows)
+		SaveData();
 }
 
 void SaveData()
